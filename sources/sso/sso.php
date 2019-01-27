c7294bd069806bded2b46495089747332dfca0fa
Yalh <yalh@yalh.net>
2019-01-27 21:12:58 +0100

    Fix install

--- sources/sso/sso.php
+++ sources/sso/sso.php
@@ -1,9 +1,22 @@
 <?php
 
-// Enable RainLoop Api and include index file 
-$_ENV['RAINLOOP_INCLUDE_AS_API'] = true;
-include '__FINAL_PATH__/app/index.php';
+if (!defined('APP_VERSION'))
+{
+	define('APP_VERSION', '1.12.1');
+	define('APP_VERSION_TYPE', 'community');
+	define('APP_INDEX_ROOT_FILE', __FILE__);
+	define('APP_INDEX_ROOT_PATH', str_replace('\\', '/', rtrim(dirname(__FILE__), '\\/').'/'));
+}
 
+if (file_exists(APP_INDEX_ROOT_PATH.'rainloop/v/'.APP_VERSION.'/include.php'))
+{
+	include APP_INDEX_ROOT_PATH.'rainloop/v/'.APP_VERSION.'/include.php';
+}
+else
+{
+	echo '[105] Missing version directory';
+	exit(105);
+}
 // Retrieve email and password
 if (isset($_SERVER['HTTP_EMAIL']) && isset($_SERVER['PHP_AUTH_PW'])) {
 	$email = $_SERVER['HTTP_EMAIL'];
